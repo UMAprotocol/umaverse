70e36b112439e1f18839274de6852a84ddae0afd
Tulun <jaykiraly@gmail.com>
2021-07-02 10:15:14 -0700

    converted mint to other comps.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- components/lsp-form/MintForm.tsx
+++ components/lsp-form/MintForm.tsx
@@ -1,24 +1,17 @@
 import React, { FC, useState } from "react";
 import {
-  FormRow,
   SmallTitle,
   TopFormWrapper,
   BottomFormWrapper,
-  BalanceRow,
   DownArrowWrapper,
-  BalanceRowToken,
   ButtonWrapper,
   MintButton,
-  iconStyles,
 } from "./LSPForm.styled";
-import EthIcon from "../../public/icons/eth-icon.svg";
-import UniswapIcon from "../../public/icons/uniswap-logo.svg";
-import Dropdown from "../dropdown";
-import TextInput from "../text-input";
-import { DropdownVariant } from "../dropdown/Dropdown";
-import { LabelPlacement } from "../text-input/TextInput";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+import LongShort from "./LongShort";
+import Collateral from "./Collateral";
 
 const MintForm: FC = () => {
   const [collateral, setCollateral] = useState("");
@@ -30,35 +23,12 @@ const MintForm: FC = () => {
     <div>
       <TopFormWrapper>
         <SmallTitle>Input</SmallTitle>
-        <FormRow>
-          {collateral === "eth" && <EthIcon style={iconStyles} />}
-          {collateral === "uniswap" && <UniswapIcon style={iconStyles} />}
-          <Dropdown
-            setValue={setCollateral}
-            variant={"coin" as DropdownVariant}
-            defaultValue={{ label: "ETH", value: "eth" }}
-            items={[
-              {
-                label: "ETH",
-                value: "eth",
-              },
-              { label: "UNI", value: "uniswap" },
-            ]}
-          />
-          <TextInput
-            label="collateral"
-            labelPlacement={"overlap" as LabelPlacement}
-            placeholder="0.0"
-            value={amount}
-            setValue={setAmount}
-            width={230}
-          />
-        </FormRow>
-        <BalanceRow>
-          <div>
-            <span>Your Balance 0.7431</span> <span>Max</span>
-          </div>
-        </BalanceRow>
+        <Collateral
+          collateral={collateral}
+          setCollateral={setCollateral}
+          amount={amount}
+          setAmount={setAmount}
+        />
       </TopFormWrapper>
       <DownArrowWrapper>
         <FontAwesomeIcon icon={faArrowDown} />
@@ -66,30 +36,12 @@ const MintForm: FC = () => {
 
       <BottomFormWrapper>
         <SmallTitle>Output</SmallTitle>
-        <FormRow>
-          <TextInput
-            label="long token"
-            labelPlacement={"overlap" as LabelPlacement}
-            placeholder="0.0"
-            value={longTokenAmount}
-            setValue={setLongTokenAmount}
-          />
-          <TextInput
-            label="short token"
-            labelPlacement={"overlap" as LabelPlacement}
-            placeholder="0.0"
-            value={shortTokenAmount}
-            setValue={setShortTokenAmount}
-          />
-        </FormRow>
-        <BalanceRowToken>
-          <div>
-            <span>Your Balance 123.45 </span>
-          </div>
-          <div>
-            <span>Your Balance 567.89</span>
-          </div>
-        </BalanceRowToken>
+        <LongShort
+          longTokenAmount={longTokenAmount}
+          setLongTokenAmount={setLongTokenAmount}
+          shortTokenAmount={shortTokenAmount}
+          setShortTokenAmount={setShortTokenAmount}
+        />
       </BottomFormWrapper>
       <ButtonWrapper>
         <MintButton>Mint</MintButton>
