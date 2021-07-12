022f96b01110b5cd075e1c2053b5361ba0710107
Francesco <francesco@umaproject.org>
2021-07-12 19:23:46 +0200

    feat(umaverse): add error reporting  (#6)
    * feat(umaverse): add sentry error reporting
    
    Signed-off-by: Gamaranto <francesco@umaproject.org>
    
    * feat(umaverse): add error for failed cms fetching
    
    Signed-off-by: Gamaranto <francesco@umaproject.org>

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,9 +1,18 @@
+import * as Sentry from "@sentry/nextjs";
+import { ContentfulSynth } from "./contentful";
+
 export function errorFilter(value: unknown | Error): boolean {
   if (value instanceof Error) {
-    // TODO: Have a proper error reporting mechanism
-    console.log(`Errored at`);
-    console.log(value);
+    Sentry.captureException(value);
     return false;
   }
   return true;
+}
+
+export class SynthFetchingError extends Error {
+  public constructor(message: string, synth: ContentfulSynth) {
+    super();
+    this.name = this.constructor.name;
+    this.message = `Failed to fetch synth ${synth.shortDescription} with address: ${synth.address}. Got error ${message}`;
+  }
 }
