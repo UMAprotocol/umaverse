2509e79ff2e742b93e598f054e50132c3f9223f8
Francesco <francesco@umaproject.org>
2021-07-12 12:22:46 +0200

    feat(umaverse): add favicon (#5)
    Signed-off-by: Gamaranto <francesco@umaproject.org>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,6 +17,28 @@ export const Layout: React.FC<Props> = ({
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      {/* Favicon links made with  https://realfavicongenerator.net/ */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     <Header />
     {children}
