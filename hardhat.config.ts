e1a50250208993bd31447a785db62293656f6b95
Tulun <jaykiraly@gmail.com>
2021-09-21 14:11:33 -0700

    ts config.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      hardfork: "london",
+      gasPrice: "auto",
+      initialBaseFeePerGas: 1_000_000_000,
+    },
+  },
+};
