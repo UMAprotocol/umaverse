bee4d036a9834d91539f7f8d7a5a37d38a8278f5
Tulun <jaykiraly@gmail.com>
2021-09-28 10:40:55 -0700

    format unit test.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ utils/tests/format.test.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+import { formatTvlChange } from "../format";
+
+describe("formatTvlChange", () => {
+  it("Returns a '-' when dayTvl is 0", () => {
+    const value = formatTvlChange(0, "10000");
+    expect(value).toBe("-");
+  });
+
+  it("Returns 0 when dayTvl is NaN", () => {
+    const value = formatTvlChange(NaN, "10000");
+    expect(value).toBe(0);
+  });
+
+  it("Returns a number when dayTvl is > 0", () => {
+    const x2 = "200";
+    const x1 = 100;
+    const growthRatePercent = ((Number(x2) - x1) / x1) * 100;
+    const value = formatTvlChange(x1, x2);
+    expect(value).toBe(growthRatePercent);
+  });
+});
