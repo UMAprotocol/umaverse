2e021a33719a6f936ebe0cd0d2f937d1dff96bed
evaldofelipe <contato@evaldofelipe.com>
2021-10-14 17:09:46 +0200

    .circleci/nvm_config.sh

--- .circleci/nvm_config.sh
+++ .circleci/nvm_config.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
 echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
-echo ' [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
+echo '' >> $BASH_ENV
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
