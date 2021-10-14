a2c09aa1933e54f9267cf1fa503efafb46de7ded
evaldofelipe <contato@evaldofelipe.com>
2021-10-14 17:36:16 +0200

    Update nvm install
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

--- .circleci/nvm_config.sh
+++ .circleci/nvm_config.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-set -o errexit
-set -o nounset
-
-echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
+export NVM_DIR="/opt/circleci/.nvm"
+ [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
