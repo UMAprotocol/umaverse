cfa9eb6f14e6fd999230fb052425c9f3067efa0f
Alex Gaines <againes@umaproject.org>
2022-02-25 10:13:36 -0600

    Updates based on review suggestions
    Signed-off-by: Alex Gaines <againes@umaproject.org>

--- utils/defillama.ts
+++ utils/defillama.ts
@@ -14,14 +14,30 @@ interface TvlData {
   totalLiquidityUSD: number;
 }
 
-export async function getDefillamaTvl() {
+export async function getTvl(url: string = baseUrl) {
+  const { data } = await axios.get(url);
+  const defillamaTvlData: TvlData[] = data.tvl.slice(-1);
+  const latestTvl = defillamaTvlData[0].totalLiquidityUSD;
+  return latestTvl;
+}
+
+export async function getPercentChange(url: string = baseUrl) {
+  const { data } = await axios.get(url);
+  const defillamaTvlData: TvlData[] = data.tvl.slice(-2);
+  const latestTvl = defillamaTvlData[1].totalLiquidityUSD;
+  const previousDayTvl = defillamaTvlData[0].totalLiquidityUSD;
+  return {
+    latestTvl,
+    previousDayTvl,
+  };
+}
+
+export async function getDefillamaTvl(url: string[] = defillamaUrls) {
   const totalLatestTvl = [];
 
   for (let i = 0; i < defillamaUrls.length; i++) {
-    const { data } = await axios.get(defillamaUrls[i]);
-    const defillamaTvlData: TvlData[] = data.tvl.slice(-1);
-    const projectTvl = defillamaTvlData[0].totalLiquidityUSD;
-    totalLatestTvl.push(projectTvl);
+    const todayTvl = await getTvl(url[i]);
+    totalLatestTvl.push(todayTvl);
   }
   const latestTvl = totalLatestTvl.reduce(function (totalLatestTvl, a) {
     return totalLatestTvl + a;
@@ -29,17 +45,14 @@ export async function getDefillamaTvl() {
   return latestTvl;
 }
 
-export async function getDefillamaPercentChange() {
+export async function getDefillamaPercentChange(url: string[] = defillamaUrls) {
   const todayTvl = [];
   const yesterdayTvl = [];
 
   for (let i = 0; i < defillamaUrls.length; i++) {
-    const { data } = await axios.get(defillamaUrls[i]);
-    const defillamaTvlData: TvlData[] = data.tvl.slice(-2);
-    const latestTvl = defillamaTvlData[1].totalLiquidityUSD;
-    const previousDayTvl = defillamaTvlData[0].totalLiquidityUSD;
-    todayTvl.push(latestTvl);
-    yesterdayTvl.push(previousDayTvl);
+    const llamaTvl = await getPercentChange(url[i]);
+    todayTvl.push(llamaTvl.latestTvl);
+    yesterdayTvl.push(llamaTvl.previousDayTvl);
   }
   const latestTvl = todayTvl.reduce(function (todayTvl, a) {
     return todayTvl + a;
@@ -47,28 +60,10 @@ export async function getDefillamaPercentChange() {
   const previousDayTvl = yesterdayTvl.reduce(function (yesterdayTvl, b) {
     return yesterdayTvl + b;
   }, 0);
-
   const defillamaPercentChange =
     Math.round(((latestTvl - previousDayTvl) / previousDayTvl) * 1000) / 10;
   return defillamaPercentChange;
 }
-
-// export async function getDefillamaTvl(url: string = baseUrl) {
-//   const { data } = await axios.get(url);
-//   const defillamaTvlData: TvlData[] = data.tvl.slice(-1);
-//   const latestTvl = defillamaTvlData[0].totalLiquidityUSD;
-//   return latestTvl;
-// }
-
-// export async function getDefillamaPercentChange(url: string = baseUrl) {
-//   const { data } = await axios.get(url);
-//   const defillamaTvlData: TvlData[] = data.tvl.slice(-2);
-//   const latestTvl = defillamaTvlData[1].totalLiquidityUSD;
-//   const previousDayTvl = defillamaTvlData[0].totalLiquidityUSD;
-//   const defillamaPercentChange =
-//     Math.round(((latestTvl - previousDayTvl) / previousDayTvl) * 1000) / 10;
-//   return defillamaPercentChange;
-// }
 
 export async function getDefillamaStats(url: string = baseUrl) {
   const { data } = await axios.get(url);
